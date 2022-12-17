ce534c51ed1b853041621fdf23149249b739b529
yaochu21 <yaochu1828@gmail.com>
2022-12-16 22:23:26 -0600

    URL input, submission, and async handling setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import StepOne from "./Forms/StepOne";
+import StepTwo from "./Forms/StepTwo";
+import StepThree from "./Forms/StepThree";
+import StepFour from "./Forms/StepFour";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        position: "absolute",
+        marginLeft: "10rem",
+        marginTop: "10rem",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <StepOne />
     </div>
   );
 }
---
+++ src/Forms/StepOne.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import StepContainer from "../UIElements/StepContainer";
+import styled, { css } from "styled-components";
+import ReactLoading from 'react-loading';
+
+const StepOne = (props) => {
+  const [urlInput, setURLInput] = useState("Enter url here...");
+  const [isDefault, setIsDefault] = useState(true);
+  const [urlInvalid, setURLInvalid] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const onChangeHandler = (event) => {
+    setURLInput(event.target.value);
+    if (urlInvalid) {
+      setURLInvalid(false);
+    }
+  };
+
+  const onFocusHandler = (event) => {
+    if (isDefault || urlInvalid) {
+      setURLInput("");
+      setIsDefault(false);
+      setURLInvalid(false);
+    }
+  };
+
+  const onBlurHandler = (event) => {
+    if (urlInput.length <= 0) {
+      setIsDefault(true);
+      setURLInput("Enter url here...");
+    }
+  };
+
+  const urlSubmitHandler = async (event) => {
+    console.log("submitting url");
+
+    // string validation
+    if (!validateHttpURL(urlInput)) {
+      console.log("not a valid http url");
+      setURLInvalid(true);
+      return;
+    }
+    
+    const api = "";
+    const data = { url: urlInput };
+
+    setIsFetching(true);
+    fetch(api, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+
+        if (!response.ok) {
+            throw new Error("invalid response: status code " + response.status);
+        }
+
+        const contentType = response.headers.get("content-type");
+        if (!contentType || (contentType.indexOf("application/json") < 0)) {
+          throw new TypeError("fetched data was not json");
+        }
+
+        return response.json();
+      })
+      .then((jsonData) => {
+
+        setIsFetching(false);
+
+        // process data (redux)
+
+      })
+      .catch(handleFetchError);
+  };
+
+  const handleFetchError = (error) => {
+    console.log("Error during fetch: " + error);
+    setIsFetching(false);
+  };
+
+  const handleStatusError = (status) => {};
+
+  const validateHttpURL = (urlString) => {
+    try {
+      let urlObject = new URL(urlString);
+      if (urlObject.protocol.indexOf("http") != 0) {
+        return false;
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  return (
+    <StepOneContainer>
+      <URLInput
+        value={urlInput}
+        onChange={onChangeHandler}
+        onFocus={onFocusHandler}
+        onBlur={onBlurHandler}
+        isDefault={isDefault}
+      ></URLInput>
+      <button onClick={urlSubmitHandler}> Submit </button>
+      {isFetching && <ReactLoading type="spinningBubbles" color="gray" width={32} height={32}/>}
+      {urlInvalid && <ErrorMessageLabel> Invalid URL </ErrorMessageLabel>}
+    </StepOneContainer>
+  );
+};
+
+const URLInput = styled.input(
+  (props) => css`
+    width: 15rem;
+    overflow: hidden;
+    ${props.isDefault &&
+    css`
+      font-style: italic;
+      color: rgba(0, 0, 0, 0.5);
+    `}
+  `
+);
+
+const ErrorMessageLabel = styled.div`
+  color: red;
+  font-size: 0.75rem;
+`;
+
+const StepOneContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 1.5rem;
+  gap: 0.5rem;
+  justify-content: center;
+  align-items: center;
+`
+
+export default StepOne;
